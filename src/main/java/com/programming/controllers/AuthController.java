336d5a9de6c511bd2d59617077b231525f042186
Lokesh Vishnoi <lokeshvishnoi1@gmail.com>
2021-01-14 16:14:22 +0530

    securty and login session

--- src/main/java/com/programming/controllers/AuthController.java
+++ src/main/java/com/programming/controllers/AuthController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AuthController {
 
-	@RequestMapping("/admin")
+	@RequestMapping("/login")
 	String admin() {
-		return "admin";
+		return "app.login";
 	}
 }
